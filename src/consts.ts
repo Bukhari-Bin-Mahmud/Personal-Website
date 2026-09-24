94a1140f507ae9a9f9444950495851018cede9e9
Bukhari Bin Mahmud <bukhari@example.com>
2026-09-25 02:51:12 +0600

    seo: Appended Software Engineer to site title for better keyword targeting

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Bukhari Bin Mahmud';
+export const SITE_TITLE = 'Bukhari Bin Mahmud | Software Engineer';
 export const SITE_DESCRIPTION = 'Personal website, portfolio, and blog of Bukhari Bin Mahmud, a Computer Science student and software engineer.';
